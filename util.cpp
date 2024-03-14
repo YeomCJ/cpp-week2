a9110b65c889d851c4205efafeff1fafe92f8f72
YeomCJ <ycj0010@naver.com>
2024-03-14 16:31:46 +0900

    fix: 정답의 숫자가 중복되지 않도록 변경

--- util.cpp
+++ util.cpp
@@ -7,7 +7,16 @@ int util() {
     std::mt19937 gen(time(NULL));
     std::uniform_int_distribution<int> dis(0,9);
 
-    int a = dis(gen), b = dis(gen), c = dis(gen);
+    int a = dis(gen), b, c, k = 1;
+    while (k) {
+        b = dis(gen);
+        if (a != b) {
+            while(k) {
+                c = dis(gen);
+                if (c != a && c != b) k--;
+            }
+        } 
+    }
     int sum = a * 100 + (b *10) + c;
     
     return sum;
